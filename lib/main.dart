f4f0d360600e434fddf6b1c41846146669bd6bb2
Yudianto Sujana <yudianto.sujana@staff.uns.ac.id>
2022-11-14 20:08:46 +0700

    First

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:weather/screens/loading_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
